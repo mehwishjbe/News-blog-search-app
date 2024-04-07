fb04d705ea4cee96c99d22a69942981a8cc8b2fd
mehwishjbe <mehwish.jbe@gmail.com>
2024-04-07 18:47:57 +0200

    try catch statement js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const apiKey = 'c9cd0b0a2fa84f9c821a9d23453c8bf8';
+const apiKey = "c9cd0b0a2fa84f9c821a9d23453c8bf8";
 
 const blogContainer = document.getElementById("blog-card");
 
-function fetchRandomNews(){
-    try{
-
-    } catch(error){
-        
-    }
+function fetchRandomNews() {
+  try {
+  } catch (error) {
+    console.error("Error fetching random news", error);
+    return [];
+  }
 }
