cebf7b7512467761750e8c5b72c60128c0fbf235
mehwishjbe <mehwish.jbe@gmail.com>
2024-04-07 18:45:31 +0200

    try catch js

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const apiKey = 'c9cd0b0a2fa84f9c821a9d23453c8bf8';
 
 const blogContainer = document.getElementById("blog-card");
 
+function fetchRandomNews(){
+    try{
+
+    } catch(error){
+        
+    }
+}
