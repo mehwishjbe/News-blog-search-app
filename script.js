51944052442fd25ab619e91c40d9b65c88f509a9
mehwishjbe <mehwish.jbe@gmail.com>
2024-04-07 18:58:04 +0200

    url in async js

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const apiKey = "c9cd0b0a2fa84f9c821a9d23453c8bf8";
 
 const blogContainer = document.getElementById("blog-card");
 
-function fetchRandomNews() {
+async function fetchRandomNews() {
   try {
+    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize10&apiKey=${apiKey}`;
   } catch (error) {
     console.error("Error fetching random news", error);
     return [];
