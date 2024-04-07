52330fcd2352d0dc2e133081bf26716fee2d7f13
mehwishjbe <mehwish.jbe@gmail.com>
2024-04-07 20:07:31 +0200

    changing in title element with condition js

--- script.js
+++ script.js
@@ -25,7 +25,11 @@ document.addEventListener("DOMContentLoaded", (event) => {
       img.src = article.urlToImage;
       img.alt = article.title;
       const title = document.createElement("h2");
-      title.textContent = article.title;
+      const truncatedTitle =
+        article.title.length > 30
+          ? article.title.slice(0, 30) + "...."
+          : article.title;
+      title.textContent = truncatedTitle;
       const description = document.createElement("p");
       description.textContent = article.description;
       blogCard.appendChild(img);
